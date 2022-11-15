432e874d9f0fa0f04c05da5677aaa950da8412bf
Andy Walker <andy.walker@modulrfinance.com>
2022-11-15 19:41:13 +0000

    Initial bare-bones project

---
+++ src/main/java/com/andrewwalkerscotland/ArestedDevelopment/ArestedDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.andrewwalkerscotland.ArestedDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArestedDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArestedDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrewwalkerscotland/ArestedDevelopment/ArestedDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewwalkerscotland.ArestedDevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArestedDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
